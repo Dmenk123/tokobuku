804af7781857b9f923b3de3b37de8fed9ac8e559
Dmenk123 <rizkiyuandaa@gmail.com>
2020-01-25 09:47:37 +0700

    asfa

--- properti.sql
+++ properti.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100129
  File Encoding         : 65001
 
- Date: 21/01/2020 15:30:11
+ Date: 25/01/2020 09:46:34
 */
 
 SET NAMES utf8mb4;
@@ -27,7 +27,7 @@ CREATE TABLE `m_level_user`  (
   `keterangan_level_user` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT '',
   `aktif` int(1) NULL DEFAULT 1,
   PRIMARY KEY (`id_level_user`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of m_level_user
@@ -59,10 +59,12 @@ CREATE TABLE `m_menu`  (
 -- ----------------------------
 -- Records of m_menu
 -- ----------------------------
-INSERT INTO `m_menu` VALUES (1, 0, 'Dashboard', 'Dashboard', 'home', 'fa fa-dashboard', 1, 1, 1, 0, 0, 0);
-INSERT INTO `m_menu` VALUES (2, 4, 'Setting Menu', 'Setting Menu', 'set_menu_adm', NULL, 1, 2, 2, 1, 1, 1);
-INSERT INTO `m_menu` VALUES (3, 4, 'Setting Role', 'Setting Role', 'set_role_adm', '', 1, 2, 1, 1, 1, 1);
+INSERT INTO `m_menu` VALUES (1, 0, 'Dashboard', 'Dashboard', 'admin/dashboard', 'fa fa-dashboard', 1, 1, 1, 0, 0, 0);
+INSERT INTO `m_menu` VALUES (2, 4, 'Setting Menu', 'Setting Menu', 'admin/set_menu_adm', NULL, 1, 2, 2, 1, 1, 1);
+INSERT INTO `m_menu` VALUES (3, 4, 'Setting Role', 'Setting Role', 'admin/set_role_adm', '', 1, 2, 1, 1, 1, 1);
 INSERT INTO `m_menu` VALUES (4, 0, 'Setting (Administrator)', 'Setting', NULL, 'fa fa-gear', 1, 1, 5, 0, 0, 0);
+INSERT INTO `m_menu` VALUES (5, 0, 'Data Master', 'Data Master', ' ', 'fa fa-database', 1, 1, 2, 0, 0, 0);
+INSERT INTO `m_menu` VALUES (6, 5, 'Master Produk', 'Master Produk', 'admin/master_produk_adm', '', 1, 2, 1, 1, 1, 1);
 
 -- ----------------------------
 -- Table structure for m_user
@@ -84,7 +86,7 @@ CREATE TABLE `m_user`  (
 -- ----------------------------
 -- Records of m_user
 -- ----------------------------
-INSERT INTO `m_user` VALUES ('USR00001', 'masnur', 'kmJnZmZo', 1, NULL, 1, '2020-01-21 09:27:43', '2019-10-05 21:34:14', '2020-01-21 15:27:43');
+INSERT INTO `m_user` VALUES ('USR00001', 'masnur', 'kmJnZmZo', 1, NULL, 1, '2020-01-24 07:39:35', '2019-10-05 21:34:14', '2020-01-24 13:39:35');
 INSERT INTO `m_user` VALUES ('USR00002', 'agen', 'kmJnZmZo', 2, NULL, 1, '2019-12-03 08:42:33', '2019-11-09 19:36:13', '2020-01-21 15:03:27');
 INSERT INTO `m_user` VALUES ('USR00003', 'customer', 'kmJnZmZo', 3, NULL, 1, '2019-12-03 08:41:40', '2019-11-09 19:43:19', '2020-01-21 15:03:52');
 
@@ -104,7 +106,7 @@ CREATE TABLE `m_user_detail`  (
   `thumb_gambar_user` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'user_default_thumb.png',
   PRIMARY KEY (`id_user_detail`) USING BTREE,
   UNIQUE INDEX `id_user`(`id_user`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of m_user_detail
@@ -128,5 +130,15 @@ CREATE TABLE `t_hak_akses`  (
   CONSTRAINT `f_level_user` FOREIGN KEY (`id_level_user`) REFERENCES `m_level_user` (`id_level_user`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `t_hak_akses_ibfk_1` FOREIGN KEY (`id_menu`) REFERENCES `m_menu` (`id_menu`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of t_hak_akses
+-- ----------------------------
+INSERT INTO `t_hak_akses` VALUES (1, 1, 0, 0, 0);
+INSERT INTO `t_hak_akses` VALUES (5, 1, 0, 0, 0);
+INSERT INTO `t_hak_akses` VALUES (6, 1, 1, 1, 1);
+INSERT INTO `t_hak_akses` VALUES (4, 1, 0, 0, 0);
+INSERT INTO `t_hak_akses` VALUES (3, 1, 1, 1, 1);
+INSERT INTO `t_hak_akses` VALUES (2, 1, 1, 1, 1);
 
 SET FOREIGN_KEY_CHECKS = 1;
