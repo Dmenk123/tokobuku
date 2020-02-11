b3151bd7897a3830c7e3effaad5e3351e0d32cb1
Riski <rizkiyuandaa@gmail.com>
2020-02-11 23:02:59 +0700

    asaasasasas

--- application/controllers/Checkout.php
+++ application/controllers/Checkout.php
@@ -3,34 +3,97 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Checkout extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-
 	function __construct() {
 		parent::__construct();
 		$this->load->helper(array('url'));
 		$this->load->library(array('session', 'form_validation', 'upload', 'user_agent', 'email'));
         $this->load->helper(array('url', 'form', 'text', 'html', 'security', 'file', 'directory', 'number', 'date', 'download'));
+        $this->load->model(['mod_global']);
+        $this->load->library('cart');
+
+		if ($this->session->userdata('logged_in') != TRUE) {
+		 	redirect('register','refresh');
+		}
 	}
 
 	public function index()
 	{
-		$this->load->view('header');
-		$this->load->view('v_checkout');
+		$id_user = $this->session->userdata('id_user');
+		$where = ['m_user.id_user' => $id_user];
+		$select = "m_user.*, m_user_detail.nama_lengkap_user, m_user_detail.no_telp_user, m_user_detail.email";
+		$join = array(
+			["table" => "m_user_detail", "on" => "m_user.id_user = m_user_detail.id_user"]
+		);
+
+		$data['userdata'] = $this->mod_global->get_data_single($select, 'm_user', $where, $join);
+		
+		$arr_nama = explode(",", $data['userdata']->nama_lengkap_user);
+		$data['nama_depan'] = $arr_nama[0];
+		$data['nama_belakang'] = $arr_nama[1];
+
+		$this->load->view('v_navbar');
+		$this->load->view('v_checkout', $data);
 		$this->load->view('footer');
 	}
+
+	 function show_cart()
+    {
+        //deklarasi variabel
+        $output = '';
+        $beratTotal=0;
+        //var_dump($this->cart->contents());exit;
+        foreach ($this->cart->contents() as $items) {
+        	$subtotal = $items['price'] * (int)$items['qty'];
+            $output .='
+            	<tr>
+                  <td>
+                  	<a class="ps-product__preview" href="'.base_url('product_detail/index/'.$items['id'].'').'" target="_blank">
+                  		'.$items['name'].'
+                  	</a>
+                  </td>
+                  <td>'.$items['qty'].'</td>
+                  <td>Rp. '.number_format($subtotal,0,",",".").'</td>
+                </tr>
+            ';
+        }
+        return $output;
+    }
+ 
+    function load_cart(){ //load data cart
+        echo $this->show_cart();
+    }
+
+    function show_summary()
+    { 
+        foreach ($this->cart->contents() as $items) {
+            $link_img = $items['options']['Gambar_produk'];
+
+            $output = '
+            	<div class="ps-cart__total" align="left">
+				    <h3>Harga Total : <span class="pull-right"> Rp. '.number_format($this->cart->total(),0,",",".").'</span></h3>
+				</div>
+            ';
+        }
+        if (isset($output)) 
+        {
+            return $output;
+        }
+        else
+        {
+            $output = '<div class="ps-cart__promotion">
+            				<p> Daftar Belanja Anda Kosong </p>
+            				<hr>
+			    			<div class="form-group">
+				      			<a class="ps-btn ps-btn--gray" href="'.base_url('product_listing').'">Lanjutkan Belanja</a>
+				    		</div>
+				  		</div>';
+            return $output;
+        }
+        
+    }
+
+    function load_summary(){ //load data cart
+        echo $this->show_summary();
+    }
 
 }
--- application/views/v_checkout.php
+++ application/views/v_checkout.php
@@ -3,112 +3,78 @@
         <div class="ps-container">
           <form class="ps-checkout__form" action="do_action" method="post">
             <div class="row">
-                  <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 ">
+                  <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12 ">
                     <div class="ps-checkout__billing">
-                      <h3>Billing Detail</h3>
+                      <h3>Detail Pembelian</h3>
                             <div class="form-group form-group--inline">
-                              <label>First Name<span>*</span>
+                              <label>Nama Depan<span>*</span>
                               </label>
-                              <input class="form-control" type="text">
+                              <input class="form-control" type="text" name="fname" id="fname" value="<?= $nama_depan; ?>">
                             </div>
                             <div class="form-group form-group--inline">
-                              <label>Last Name<span>*</span>
+                              <label>Nama Belakang<span>*</span>
                               </label>
-                              <input class="form-control" type="text">
+                              <input class="form-control" type="text" name="lname" id="lname" value="<?= $nama_belakang; ?>">
                             </div>
                             <div class="form-group form-group--inline">
-                              <label>Company Name<span>*</span>
+                              <label>Email<span>*</span>
                               </label>
-                              <input class="form-control" type="text">
+                              <input class="form-control" type="email" name="email" id="email" value="<?= $userdata->email; ?>">
                             </div>
                             <div class="form-group form-group--inline">
-                              <label>Email Address<span>*</span>
+                              <label>No. Telepon<span>*</span>
                               </label>
-                              <input class="form-control" type="email">
+                              <input class="form-control" type="text" name="telp" id="telp" value="<?= $userdata->no_telp_user; ?>">
                             </div>
-                            <div class="form-group form-group--inline">
-                              <label>Company Name<span>*</span>
-                              </label>
-                              <input class="form-control" type="text">
-                            </div>
-                            <div class="form-group form-group--inline">
-                              <label>Phone<span>*</span>
-                              </label>
-                              <input class="form-control" type="text">
-                            </div>
-                            <div class="form-group form-group--inline">
-                              <label>Address<span>*</span>
-                              </label>
-                              <input class="form-control" type="text">
-                            </div>
-                      <div class="form-group">
-                        <div class="ps-checkbox">
-                          <input class="form-control" type="checkbox" id="cb01">
-                          <label for="cb01">Create an account?</label>
-                        </div>
-                      </div>
-                      <h3 class="mt-40"> Addition information</h3>
+                      <h3 class="mt-40"> Informasi Tambahan</h3>
                       <div class="form-group form-group--inline textarea">
-                        <label>Order Notes</label>
-                        <textarea class="form-control" rows="5" placeholder="Notes about your order, e.g. special notes for delivery."></textarea>
+                        <label>Catatan Pemesanan</label>
+                        <textarea class="form-control" rows="5" name="catatan" id="catatan" placeholder="Misal: Hadiah dari Mas Bram kepada Dek Shinta (Kirim ke email shintacute@gmail.com)"></textarea>
+                      </div>
+
+                      <h3 class="mt-40"> Upload Bukti Transfer</h3>
+                      <div class="form-group form-group--inline">
+                        <label>Upload Bukti</label>
+                        <input type="file" class="form-control-file" name="bukti" id="bukti">
                       </div>
                     </div>
                   </div>
-                  <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 ">
+                  <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12 ">
                     <div class="ps-checkout__order">
                       <header>
-                        <h3>Your Order</h3>
+                        <h3>Pesanan Anda</h3>
                       </header>
                       <div class="content">
                         <table class="table ps-checkout__products">
                           <thead>
                             <tr>
-                              <th class="text-uppercase">Product</th>
+                              <th class="text-uppercase">Produk</th>
+                              <th class="text-uppercase">Qty</th>
                               <th class="text-uppercase">Total</th>
                             </tr>
                           </thead>
-                          <tbody>
-                            <tr>
-                              <td>HABITANT x1</td>
-                              <td>$300.00</td>
-                            </tr>
-                            <tr>
-                              <td>Card Subtitle</td>
-                              <td>$300.00</td>
-                            </tr>
-                            <tr>
-                              <td>Order Total</td>
-                              <td>$300.00</td>
-                            </tr>
-                          </tbody>
+                          <tbody id="detail_cart"></tbody>
                         </table>
+                        <div class="ps-cart__actions summary_cart"></div>
                       </div>
                       <footer>
-                        <h3>Payment Method</h3>
+                        <h3>Metode Pembayaran / Transfer</h3>
                         <div class="form-group cheque">
-                          <div class="ps-radio">
-                            <input class="form-control" type="radio" id="rdo01" name="payment" checked>
-                            <label for="rdo01">Cheque Payment</label>
-                            <p>Please send your cheque to Store Name, Store Street, Store Town, Store State / County, Store Postcode.</p>
+                          <div class="">
+                            <p>Dimohon Transfer Ke Rekening Dibawah Ini Serta Melampirkan Upload Bukti Transfer.</p>
+                            <p>BCA : BAPAK WILLIAM - 014-1231212-12121</p>
+                            <p>MANDIRI : BAPAK WILLIAM - 021-1231212-12121</p>
+                            <p>BANK OF INDIA : BAPAK WILLIAM - 666-1231212-12121</p>
                           </div>
                         </div>
                         <div class="form-group paypal">
-                          <div class="ps-radio ps-radio--inline">
-                            <input class="form-control" type="radio" name="payment" id="rdo02">
-                            <label for="rdo02">Paypal</label>
-                          </div>
-                          <ul class="ps-payment-method">
-                            <li><a href="#"><img src="images/payment/1.png" alt=""></a></li>
-                            <li><a href="#"><img src="images/payment/2.png" alt=""></a></li>
-                            <li><a href="#"><img src="images/payment/3.png" alt=""></a></li>
-                          </ul>
-                          <button class="ps-btn ps-btn--fullwidth">Place Order<i class="ps-icon-next"></i></button>
+                          <button class="ps-btn ps-btn--fullwidth">Konfirmasi & Selesai<i class="ps-icon-next"></i></button>
                         </div>
                       </footer>
                     </div>
                     <div class="ps-shipping">
-                      <h3>FREE SHIPPING</h3>
-                      <p>YOUR ORDER QUALIFIES FOR FREE SHIPPING.<br> <a href="#"> Singup </a> for free shipping on every order, every time.</p>
+                      <h3>PERLU ANDA KETAHUI</h3>
+                      <p>Ketika Anda Selesai Melakukan Proses Checkout ini.<br> <a href="#"> Klik Disini </a> Untuk Memantau Status Pemesanan Anda, Atau pada Menu Profile Anda.</p>
                     </div>
                   </div>
             </div>
@@ -116,3 +82,12 @@
         </div>
       </div>
     </main>
+
+     <script>
+      $(document).ready(function() {
+        // Load shopping cart
+        $('#detail_cart').load("<?php echo site_url('checkout/load_cart'); ?>");
+        // Load summary cart
+        $('.summary_cart').load("<?php echo site_url('checkout/load_summary'); ?>");
+      });
+    </script>
